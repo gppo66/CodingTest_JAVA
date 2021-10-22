a9b39e9d40932ee9d93201f5c0fd98719ca6d5b0
gppo66 <gppo66@gmail.com>
2021-10-22 23:59:07 +0900

    자바 기초 뿌시기

--- maxnumber/src/maxnumber/Main.java
+++ maxnumber/src/maxnumber/Main.java
@@ -8,34 +8,18 @@ public class Main {
 	public static void main(String[] args) {
  
 		Scanner scanner = new Scanner(System.in);
-		int n = scanner.nextInt();
-		scanner.nextLine();
-		double ave = 0;
-		double[] total = new double[n];
-		double sum = 0;
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		int[] j = new int[3];
+		int n = b;
+		for(int i = 0 ; i < j.length ; i++) {			
+			j[i] = n%10;
+			n = n/10;
+		}
+		System.out.println(a*j[0]);
+		System.out.println(a*j[1]);
+		System.out.println(a*j[2]);
+		System.out.println(a*b);
 		
-		int count = 0;
-		for(int i = 0 ; i < n ; i++) {
-			String[] ss = scanner.nextLine().split(" ");
-			sum = 0;
-			double[] score = new double[ss.length-1];	
-			for(int j = 0 ; j < ss.length-1 ; j++) {
-				score[j] = Integer.parseInt(ss[j+1]);
-				sum += score[j];
-				//System.out.println("sum : " + sum);
-			}
-			ave = sum/score.length;
-			//System.out.println("ave : " + ave);
-			for(int k = 0 ; k < score.length ; k++) {
-				if(score[k] > ave) count++;
-				//System.out.println("score : " + score[k] + " count : " + count + " score.length : " + score.length);
-			}
-			total[i] = ((double)count/(double)score.length)*100.0;
-			//System.out.println("total : " + total[i]);
-			count = 0;
-		}
-		for(int i = 0 ; i < total.length ; i++) {
-			System.out.println(String.format("%.3f", total[i])+"%");
-		}
 	}
 }
